861ffe7da98a45148fdf1434981e8a83c7ee06da
Allan Siongco <allan.siongco@gmail.com>
2022-09-19 17:28:33 -0500

    Initial configuration of OpenLayers

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css';
+import Draw from 'ol/interaction/Draw';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
